0e11d5c14778c9198840ab05195c500e27a5e10d
hash <66149987+antarktidi4@users.noreply.github.com>
2020-12-07 14:37:30 -0800

    Update textCommCog.py

--- cogs/textCommCog.py
+++ cogs/textCommCog.py
@@ -18,7 +18,6 @@ class textCommands(commands.Cog):
 		embed=discord.Embed(title="Help command")
 		embed.add_field(name="text command", value="gaytest - тест на гея\ndickometr - размер твоего гиганта\noppr @nick - унижение чела", inline=True)
 		embed.add_field(name="parse command", value="anec - анекдот\nmeme - рандомный мем\nrhentai - рандом пик хентая\ndhentai {tag} - пик хентая по тегу", inline=True)
-		embed.set_footer(text=f"help for {ctx.message.author.mention}")
 		await ctx.send(embed=embed)
 
 
