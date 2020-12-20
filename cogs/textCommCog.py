022497b7436224794ef98e609c99828820f3add4
hash <66149987+antarktidi4@users.noreply.github.com>
2020-12-20 15:37:58 -0800

    хуйс
    да.

--- cogs/textCommCog.py
+++ cogs/textCommCog.py
@@ -17,8 +17,7 @@ class textCommands(commands.Cog):
 		embed=discord.Embed(title="Help command")
 		embed.add_field(name="text command", value="gaytest - тест на гея\ndickometr - размер твоего гиганта\noppr @nick - унижение чела\nct {text} - клоунский текст", inline=True)
 		embed.add_field(name="parse command", value="anec - анекдот\nmeme - рандомный мем\nrhentai - рандом пик хентая\ndhentai {tag} - пик хентая по тегу", inline=True)
-		embed.add_field(name="games", value="ttt @nick - крестики нолики", inline=True)
-		embed.add_field(name="games", value="rHelp - помощь по русской рулетке", inline=True)
+		embed.add_field(name="games", value="ttt @nick - крестики нолики\nrHelp - помощь по русской рулетке", inline=True)
 		await ctx.send(embed=embed)
 
 
