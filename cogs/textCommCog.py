196a8a1b6c9bb48acac4f6715675693d91617b0b
hash <66149987+antarktidi4@users.noreply.github.com>
2020-12-21 11:31:54 -0800

    up add
    добавил погоняло, ексепт в оппр

--- cogs/textCommCog.py
+++ cogs/textCommCog.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from random import randint
+from random import randint, choice
 import discord
 
 
@@ -15,11 +15,10 @@ class textCommands(commands.Cog):
 	@commands.command()
 	async def help(self, ctx):
 		embed=discord.Embed(title="Help command")
-		embed.add_field(name="text command", value="gaytest - тест на гея\ndickometr - размер твоего гиганта\noppr @nick - унижение чела\nct {text} - клоунский текст", inline=True)
-		embed.add_field(name="parse command", value="anec - анекдот\nmeme - рандомный мем\nrhentai - рандом пик хентая\ndhentai {tag} - пик хентая по тегу", inline=True)
-		embed.add_field(name="games", value="ttt @nick - крестики нолики\nrHelp - помощь по русской рулетке", inline=True)
+		embed.add_field(name="text command", value="gaytest - *тест на гея*\ndickometr - *размер твоего гиганта*\noppr @nick - *унижение чела*\nct {text} - *клоунский текст*\nup @nick(опц.) - *погоняло*", inline=True)
+		embed.add_field(name="parse command", value="anec - *анекдот*\nmeme - *рандомный мем*\nrhentai - *рандом пик хентая*\ndhentai {tag} - *пик хентая по тегу*", inline=True)
+		embed.add_field(name="games", value="ttt @nick - *крестики нолики*\nrHelp - *помощь по русской рулетке*", inline=True)
 		await ctx.send(embed=embed)
-
 
 	@commands.command(name = 'rHelp')
 	async def rHelp(self, ctx):
@@ -68,10 +67,19 @@ class textCommands(commands.Cog):
 		msg = await ctx.message.channel.fetch_message(ctx.message.id)
 		chance = randint(1, 4)
 		text = f'{ctx.message.author.mention} унизил {member.mention}' if chance != 4 else f'{ctx.message.author.mention} попытался унизить {member.mention}, но обосрался и ушёл с позором'
+		try:
+			await msg.delete()
+		except:
+			pass
 		await ctx.send(text)
-		await msg.delete()
+
+	@commands.command(name = 'up')
+	async def up(self, ctx, member: discord.Member = None):
+		user = ctx.message.author.mention if member is None else member.mention
+		p = ['попущенец', 'педофилыч', 'хуйс', 'король', 'пиздоблядка', 'хохол', 'вор', 'чиркаш', 'депутат']
+		await ctx.send(f'{user} получает заслуженное звание "{choice(p)}"')
 
 
-		
+
 def setup(client):
 	client.add_cog(textCommands(client))
