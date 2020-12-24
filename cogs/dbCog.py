6f4c74907343481038d27367be9bd448c7488a8e
hash <66149987+antarktidi4@users.noreply.github.com>
2020-12-24 03:29:23 -0800

    Update dbCog.py

--- cogs/dbCog.py
+++ cogs/dbCog.py
@@ -77,6 +77,7 @@ async def updateLvl(user, channel):
 		
 		
 def UI(id):
+	UserLvl.updateData(id)
 	cursor.execute(f"SELECT * FROM users WHERE userID = '{id}'")
 	for userInfo in cursor.fetchall():
 		userInfo = userInfo
@@ -87,12 +88,12 @@ def addExp(startExp, nextLvlExp, id):
 	db.commit()
 
 def updateAlias(id, alias):
-	updateData(id)
+	UserLvl.updateData(id)
 	cursor.execute(f"UPDATE users SET useralias = '{alias}' WHERE userID = '{id}'")
 	db.commit()
 
 def removeAlias(id):
-	updateData(id)
+	UserLvl.updateData(id)
 	cursor.execute(f"UPDATE users SET useralias = NULL WHERE userID = '{id}'")
 	db.commit()
 
