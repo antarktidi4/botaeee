a5c4cfb56e574e4e18e6f53f17faaf330cc93b94
hash <66149987+antarktidi4@users.noreply.github.com>
2021-01-11 09:37:10 -0800

    1.1
    добавлен мой айди что бы релоалить в других каналах

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ client.remove_command('help')
 @client.command()
 async def load(ctx, extension):
 	role = discord.utils.get(ctx.author.guild.roles, name="Власть")
-	if role in ctx.message.author.roles:
+	if role in ctx.message.author.roles or ctx.message.author.id == 305715782732480512:
 		client.load_extension(f"cogs.{extension}")
 		await ctx.send(f'{extension} is loaded :white_check_mark:')
 
 @client.command()
 async def unload(ctx, extension):
 	role = discord.utils.get(ctx.author.guild.roles, name="Власть")
-	if role in ctx.message.author.roles:
+	if role in ctx.message.author.roles or ctx.message.author.id == 305715782732480512:
 		client.unload_extension(f"cogs.{extension}")
 		await ctx.send(f'{extension} is unloaded :white_check_mark:')
 
 @client.command()
 async def reload(ctx, extension):
 	role = discord.utils.get(ctx.author.guild.roles, name="Власть")
-	if role in ctx.message.author.roles:
+	if role in ctx.message.author.roles or ctx.message.author.id == 305715782732480512:
 		client.unload_extension(f"cogs.{extension}")
 		client.load_extension(f"cogs.{extension}")
 		await ctx.send(f'{extension} is reloaded :arrows_counterclockwise:')
