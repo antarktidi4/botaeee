7822d7ac43c0a2daf8ee2eb1bcd4d901aa37929d
hash <66149987+antarktidi4@users.noreply.github.com>
2021-01-14 17:41:03 -0800

    1.2
    вырезано оповещение старта, бтв функция для тестов, ага.

--- main.py
+++ main.py
@@ -31,20 +31,15 @@ async def reload(ctx, extension):
 		client.load_extension(f"cogs.{extension}")
 		await ctx.send(f'{extension} is reloaded :arrows_counterclockwise:')
 
-cogsList = 'bot is ready!\n'
+
 for files in os.listdir('./cogs'):
 	if files.endswith('.py'):
 		try:
 			client.load_extension(f'cogs.{files[:-3]}')
-			cogsList += f'  - *{files[:-3]}* has loaded :white_check_mark:\n'
-		except:
-			cogsList += f'  - *{files[:-3]}* has no loaded :negative_squared_cross_mark:\n'
+
 
 @client.event
 async def on_ready():
-	guild = client.get_guild(778169282655551498)
-	channel = discord.utils.get(guild.channels, name='test')
-	await channel.send(cogsList)
 	await client.change_presence(status=discord.Status.idle, activity=Activity(name='за всеми', type=ActivityType.watching))
 token = os.environ.get('token')
 client.run(token)
