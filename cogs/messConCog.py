3b9269725062df76a04bb28362645893858a7ad3
hash <66149987+antarktidi4@users.noreply.github.com>
2021-02-06 08:28:10 +0700

    upd. 3.8.4
    fix pic del

--- cogs/messConCog.py
+++ cogs/messConCog.py
@@ -1,5 +1,6 @@
-import discord, datetime
+import discord
 from discord.ext import commands
+
 
 
 class messageControlCog(commands.Cog):
@@ -10,36 +11,31 @@ class messageControlCog(commands.Cog):
 	async def on_raw_message_delete(self, payload: discord.RawReactionActionEvent):
 		if payload.cached_message.author.bot is False:
 
-			cached_message = payload.cached_message.content
+			cachedMessText = payload.cached_message.content
 			author = payload.cached_message.author
 
 			try:
-				embed=discord.Embed(title='Deleted message:', description = cached_message, color=0xff00f6)
+				embed=discord.Embed(title='Deleted Message:', description = cachedMessText, color=0xff00f6)
 				embed.set_author(name = author, icon_url = author.avatar_url)
-
-				if payload.cached_message.attachments != []:
-					embed.set_image(url = cached_message.attachments[0].url)
+				embed.set_image(url = payload.cached_message.attachments[0].proxy_url)
 
 				await payload.cached_message.channel.send(embed = embed)
-
-			except AttributeError:
-				await payload.cached_message.channel.send(f'{author.mention} delete "{cached_message}"')
+			except:
+				await payload.cached_message.channel.send(f'{author.mention} ' + cachedMessText)
 
 
 	@commands.Cog.listener()
 	async def on_raw_message_edit(self, payload: discord.RawReactionActionEvent):
-		global exceptList
 		if payload.cached_message.author.bot is False:
 
 			cached_message = payload.cached_message.content
 			data = payload.data['content']
 			author = payload.cached_message.author
-
+			text = "edit '{0}' to '{1}'".format(cached_message, data)
 			dataL = len(data)
 			cachL = len(cached_message)
 
 			if dataL*5 <= cachL:
-				text = "edit '{0}' to '{1}'".format(cached_message, data)
 				try:
 					embed=discord.Embed(title='Edit message:', description = text, color=0xff00f6)
 					embed.set_author(name = author, icon_url = author.avatar_url)
