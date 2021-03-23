a5e2b73af28bf72b6ba1bb77ae8c9b09242d522b
hash <66149987+antarktidi4@users.noreply.github.com>
2021-03-23 02:17:20 -0700

    upd 3.8.9
    added eblantraslator

--- cogs/parseCommCog.py
+++ cogs/parseCommCog.py
@@ -41,12 +41,41 @@ class parseCommands(commands.Cog):
 		except:
 			await ctx.send('попробуй ещё раз')
 
-
 	@commands.command(name = 'avatar')
 	async def avatar(self, ctx, member: discord.Member = None):
 		user = ctx.author if member is None else member
 		embed = discord.Embed(title = user.name, color=0xff00f6)
 		embed.set_image(url = user.avatar_url)
+		await ctx.send(embed = embed)
+
+	@commands.command(pass_context = True, aliases = ['eblanTranslator', 'et', 'eTranslator'])
+	async def eblanTranslator(self, ctx):
+		eng = list("qwertyuiop[]asdfghjkl;'zxcvbnm,.")
+		ru = list("йцукенгшщзхъфывапролджэячсмитьбю")
+		engToRu = dict(zip(eng, ru))
+		ruToEng = dict(zip(ru, eng))
+		message = await ctx.message.channel.fetch_message(ctx.message.reference.message_id)
+
+		for chr in message.content.lower():
+			if chr in engToRu:
+				language = engToRu
+				break
+			elif chr in ruToEng:
+				language = ruToEng
+				break
+
+		lanName = [k for k, v in locals().items() if v is language][0] # чё тут вообще за пиздец, лол
+		output = ''
+
+		for chr in message.content.lower():
+			try:
+				output += eval(f'{language}[chr]') if chr in eng or chr in ru else chr
+			except BaseException as e:
+				await ctx.send(f'error: {e}')
+				return 0
+
+		embed = discord.Embed(title = f'{lanName} message:', description = output, color=0xff00f6)
+		embed.set_author(name = message.author, icon_url = message.author.avatar_url)
 		await ctx.send(embed = embed)
 
 	@commands.command(name = 'token')
